1e77824c33f04697d59e0b2a9b723ee32aa9b9f8
echoesonmars <aleemzhaan@gmail.com>
2026-03-27 18:25:08 +0500

    add: design and styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
+
+const didactGothic = localFont({
+  src: "../public/font/DidactGothic.ttf",
+  variable: "--font-sans",
+  display: "swap",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${didactGothic.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
